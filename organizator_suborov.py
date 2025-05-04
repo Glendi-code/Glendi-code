6b8a2dfcaf285995564aff58a7fcd8158a0792ca
Glendi-code <misko.vojacek@gmail.com>
2025-05-04 17:36:32 +0200

    pridal som funkcie, ktore vytvoria folder ku kazdej pripone a vsetky subory s danou priponou do folderu

--- organizator_suborov.py
+++ organizator_suborov.py
@@ -1,7 +1,8 @@
-import os
+import os, shutil
 from pathlib import Path
 
 ext_num = []
+new_paths = []
 
 dir_str = input("Paste a dircetory from your PC: ")
 dir_path = Path(dir_str)
@@ -16,3 +17,18 @@ for file in files:
         ext_num.append(ext)
 
 print(ext_num)
+
+for ext in ext_num:
+    new_path = dir_path.joinpath(ext[1:] if ext.startswith('.') else ext)
+    new_paths.append(new_path)
+    os.makedirs(new_path, exist_ok=True)
+
+for file in files:
+    src_file = Path(os.path.join(dir_path, file))
+    if src_file.is_file():
+        name, ext = os.path.splitext(file)
+        dest_folder = new_paths[ext_num.index(ext)]
+        shutil.move(str(src_file), str(dest_folder))
+
+
+
