fb98efe48270a1f0faa174d746a2ef2e7f8ac849
Glendi-code <misko.vojacek@gmail.com>
2025-05-04 16:57:30 +0200

    pridal som funkciu na zratanie a zozbieranie pripon suborov

--- organizator_suborov.py
+++ organizator_suborov.py
@@ -1,8 +1,18 @@
 import os
 from pathlib import Path
 
-dir_str = input("Paste a dircetory from your PC: ")
-if os.path.exists(dir_str):
-    dir_path = Path(dir_str)
-    os.listdir(dir_path)
+ext_num = []
 
+dir_str = input("Paste a dircetory from your PC: ")
+dir_path = Path(dir_str)
+
+if dir_path.is_dir() and os.path.exists(dir_path):
+    files = os.listdir(dir_path)
+    print(files)
+
+for file in files:
+    name, ext = os.path.splitext(file)
+    if ext not in ext_num:
+        ext_num.append(ext)
+
+print(ext_num)
